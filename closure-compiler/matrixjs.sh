e51305700894c3fba8d606fd95dff3479753d4fc
Ingo Bürk <ingo.buerk@tngtech.com>
2013-04-13 19:09:33 +0200

    Added closure compiler stuff

---
+++ closure-compiler/matrixjs.sh
@@ -0,0 +1,9 @@
+java -jar compiler.jar \
+     --js closure-library/closure/goog/base.js \
+     --js ../matrix.js \
+     --js_output_file ../matrix.min.js \
+     --compilation_level ADVANCED_OPTIMIZATIONS \
+     --generate_exports \
+     --process_closure_primitives true \
+     --manage_closure_dependencies true \
+     --warning_level VERBOSE
