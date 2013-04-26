f19a85f942ef1fe374735061d4b93f58e9a47bce
Ingo Bürk <ingo.buerk@tngtech.com>
2013-04-26 10:14:02 +0200

    Removed compiler flag

--- scripts/closure-compiler/matrixjs.sh
+++ scripts/closure-compiler/matrixjs.sh
@@ -2,6 +2,5 @@ java -jar compiler.jar \
      --js closure-library/closure/goog/base.js \
      --js ../../matrix.js \
      --js_output_file ../../matrix.min.js \
-     --generate_exports \
      --process_closure_primitives true \
      --manage_closure_dependencies true \
